ad543eb15384fc29ea7fb8350ee6454363754f51
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-02-19 12:21:28 +0300

    fix:add error handler, change unary plus to Number()

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@ function getRandomNumber(min, max) {
   if (min === max) {
     return min;
   } else if (min > max) {
-    return 'Incorrect range. Your minimun number is greater than maximum';
+    throw new Error('Incorrect range. Your minimun number is greater than maximum');
   }
   return (Math.random() * (max - min + 1) + min);
 }
 
 function getRoundedRandomNumber(min, max) {
-  return isNaN(getRandomNumber(min, max)) ? 'Incorrect range' : Math.floor(getRandomNumber(min, max));
+  return Math.floor(getRandomNumber(min, max));
 }
 
 function getRandomNumberWithFloat(min,max,floatNumber = 1) {
-  return isNaN(getRandomNumber(min, max)) ? 'Incorrect range' : +(getRandomNumber(min, max).toFixed(floatNumber));
+  return Number(getRandomNumber(min, max).toFixed(floatNumber));
 }
 
 getRoundedRandomNumber(1, 10);
