35095713f9025d6b78d109c4e710d67a2286b25a
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-03-04 14:27:29 +0300

    fix: create construcor to get Ad, fix dublicate entries in features array

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ const BUILDING_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const CHEKIN_TIME = ['12:00', '13:00', '14:00'];
 const CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
-const ADS = [];
-const ADS__COUNT = 10;
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const ADS_COUNT = 10;
 const MAXIMUM_GUESTS = 10;
 const MAXIMUM_ROOMS = 10;
 const MAXIMUM_PRICE = 10000;
@@ -31,42 +30,42 @@ const getRandomNumberWithFloat = (min,max,floatNumber = 1) => Number(getRandomNu
 function getRandomArray(source) {
   let array = [];
   const max = getRoundedRandomNumber(1, source.length);
-  for (let i = 0; array.length <= max; i++){
-    array = [...new Set(array)];
-    array.push(source[getRoundedRandomNumber(0, source.length-1 )]);
+  while (array.length < max) {
+    let element = source[getRoundedRandomNumber(0, source.length-1)];
+    if(!array.includes(element)) {
+      array.push(element);
+    }
   }
   return array;
 }
 
 const getRandomArrayElement = (elements) => elements[getRoundedRandomNumber(0, elements.length - 1)];
 
-function craeateAd (count) {
-  for (let i = 1; i <= count; i++) {
-    const lat = getRandomNumberWithFloat(MINIMUM_LAT, MAXIMUM_LAT, 5);
-    const lng = getRandomNumberWithFloat(MINIMUM_LNG, MAXIMUM_LNG, 5);
-    ADS.push( {
-      author: {
-        avatar: `img/avatars/user${i < 10 ? '0' + i : i}.png`,
-      },
-      offer: {
-        title: 'Обьявление о сдаче',
-        address: lat, lng,
-        price: getRoundedRandomNumber(1, MAXIMUM_PRICE),
-        type: getRandomArrayElement(BUILDING_TYPES),
-        rooms: getRoundedRandomNumber(1, MAXIMUM_ROOMS),
-        guests: getRoundedRandomNumber(1, MAXIMUM_GUESTS),
-        checkin: getRandomArrayElement(CHEKIN_TIME),
-        checkout: getRandomArrayElement(CHECKOUT_TIME),
-        features: getRandomArray(FEATURES),
-        description: 'Уютный домик в центре Берлина',
-        photos: getRandomArray(PHOTOS),
-      },
-      location: {
-        lat: lat,
-        lng: lng,
-      }
-    });
-  }
-}
+const craeateAd = (_elem, id) => {
+  const lat = getRandomNumberWithFloat(MINIMUM_LAT, MAXIMUM_LAT, 5);
+  const lng = getRandomNumberWithFloat(MINIMUM_LNG, MAXIMUM_LNG, 5);
+  return {
+    author: {
+      avatar: `img/avatars/user${String(++id).padStart(2, '0')}.png`,
+    },
+    offer: {
+      title: 'Обьявление о сдаче',
+      address: lat, lng,
+      price: getRoundedRandomNumber(1, MAXIMUM_PRICE),
+      type: getRandomArrayElement(BUILDING_TYPES),
+      rooms: getRoundedRandomNumber(1, MAXIMUM_ROOMS),
+      guests: getRoundedRandomNumber(1, MAXIMUM_GUESTS),
+      checkin: getRandomArrayElement(CHEKIN_TIME),
+      checkout: getRandomArrayElement(CHECKOUT_TIME),
+      features: getRandomArray(FEATURES),
+      description: 'Уютный домик в центре Берлина',
+      photos: getRandomArray(PHOTOS),
+    },
+    location: {
+      lat: lat,
+      lng: lng,
+    }
+  };
+};
 
-craeateAd(ADS__COUNT);
+const ADS = Array.from({length: ADS_COUNT}, craeateAd);
