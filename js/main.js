51a27b40f62e65905eb52b240825e0ba6fdf4956
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-03-28 12:03:07 +0300

    fix: separate open popup function into two functions

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import { showError } from './util.js';
 import './slider.js';
 import { setUserFromSubmit } from './form-validation.js';
 import { createMarker } from './map.js';
-import { openPopup } from './popup.js';
+import { successPopup, errorPopup } from './popup.js';
 import { getData } from './api.js';
 
 getData((offers) => {
   offers.slice(0, 10).forEach((point) => createMarker(point));
 }, () => showError('Не удалось получить данные. Попробуйте ещё раз'));
 
-setUserFromSubmit(() => openPopup('success'), () => openPopup('error'));
+setUserFromSubmit(successPopup, errorPopup);
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,25 @@
 import { onPopupEscKeydown } from './util.js';
+let message;
 
 const closePopup = (elementClassName) => {
   document.querySelector(`.${elementClassName}`).remove();
   document.removeEventListener('keydown', onPopupEscKeydown);
 };
 
-const openPopup = (elementClassName) => {
-  const messageTemplate = document.querySelector(`#${elementClassName}`).content;
-  const message = messageTemplate.cloneNode(true);
+const onPopupClick = (elementClassName) => {
   document.body.append(message);
   document.querySelector(`.${elementClassName}`).addEventListener('click', () => closePopup(elementClassName));
   document.addEventListener('keydown', () => onPopupEscKeydown(event, `${elementClassName}`));
 };
 
-export { openPopup, closePopup };
+const successPopup = () => {
+  message = document.querySelector('#success').content.cloneNode(true);
+  onPopupClick('success');
+};
+
+const errorPopup = () => {
+  message = document.querySelector('#error').content.cloneNode(true);
+  onPopupClick('error');
+};
+
+export { successPopup, errorPopup, closePopup };
